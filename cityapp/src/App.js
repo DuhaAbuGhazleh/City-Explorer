ea44d515f84f2fc9af1cc298f1b7d8ff33d257ea
DuhaAbuGhazleh <duhamahmoud0@gmail.com>
2021-09-14 02:18:14 +0300

    API

--- cityapp/src/App.js
+++ cityapp/src/App.js
@@ -3,92 +3,106 @@ import Location from './components/location';
 import Searchform from './components/searchform';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card
+} from 'react-bootstrap/'
 
 export class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      city_name:"",
-      type:"",
-      lat:"",
-      lon:"",
-      datetime:"",
-      discription:"",
-      showData:false,
-      weatherData:[]
+    this.state = {
+      city_name: "",
+      type: "",
+      lat: "",
+      lon: "",
+      //datetime:"",
+      //discription:"",
+      showData: false,
+      weather: []
     }
   }
-  handleLocation=(e)=>{
-    let city_name=e.target.value;
+  handleLocation = (e) => {
+    let city_name = e.target.value;
     this.setState({
-      city_name:city_name
+      city_name: city_name
     })
   }
-  handleSubmit=(e)=>{
+  handleSubmit = (e) => {
     console.log(`${process.env.REACT_APP_LOCATIONIQ_API_KEY}`);
     e.preventDefault();
-    let config={
-      method:"GET",
-      baseURL:`https://api.locationiq.com/v1/autocomplete.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city_name}`
+    let config = {
+      method: "GET",
+      baseURL: `https://api.locationiq.com/v1/autocomplete.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city_name}`
     }
-    axios (config).then(res=>{
-      let responseData=res.data[0]
+    axios(config).then(res => {
+      let responseData = res.data[0]
       this.setState({
-        city_name:responseData.display_name,
-        lon:responseData.lon,
-        lat:responseData.lat,
-        type:responseData.type,
-        datetime:responseData.datetime,
-        discription:responseData.description,
+        //city_name:responseData.display_name,
+        lon: responseData.lon,
+        lat: responseData.lat,
+        //let: responseData.searchQuery,
+        type: responseData.type,
+        //datetime:responseData.datetime,
+        //discription:responseData.description,
 
-        map:`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${responseData.lat},${responseData.lon}&zoom=7&size=400x600&format=png.`,
-        showData:true
+        map: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${responseData.lat},${responseData.lon}&zoom=7&size=400x600&format=png.`,
+        showData: true
 
       })
-     
-    }).then(()=>{
-      axios.get(`http://${process.env.REACT_APP_BACKEND_URL}/data?lat=${this.state.datetime}&lon=${this.state.discription}`)
-      .then(res=>{
-        this.setState({weatherData:res.data})
-      });
+
+    }).then(() => {
+      axios.get(`http://${process.env.REACT_APP_BACKEND_URL}/weather?lat=${this.state.lat}&lon=${this.state.lon}&city_name=${this.state.city_name}`)
+        .then(res => {
+          this.setState({ weather: res.data })
+        });
     })
-   // .catch(err => {this.setState({errorMessage: err.message});})
+   .catch(err => {this.setState({errorMessage: err.message});})
   }
 
   render() {
     return (
       <div>
         <h1>Welcome to City explorer</h1>
-        <Searchform handleLocation={this.handleLocation} handleSubmit={this.handleSubmit}/>
+        <Searchform handleLocation={this.handleLocation} handleSubmit={this.handleSubmit} />
         {
-          this.state.showData&&
-          <Location city_name={this.state.city_name}
-                    type={this.state.type}
-                    lat={this.state.lat}
-                    lon={this.state.lon}
-                    map={this.state.map}
-          />
-         
+          this.state.weather.map(item => {
+            return <>
+
+              <Card style={{ width: '18rem' }}>
+
+                <Card.Body>
+                  <Card.Title>{item.city_name}</Card.Title>
+                  <Card.Text>
+                    <h2>{item.lat}</h2>
+                    <h2>{item.lon}</h2>
+                    <h2>{item.description}</h2>
+                    <h2>{item.date}</h2>
+                  </Card.Text>
+
+                </Card.Body>
+              </Card>
+
+
+            </>
+          })
         }
-         {
-            this.state.weatherData.map(item=>{
-              return<>
-              <h2>{item.date}</h2>
-              <h2>{item.lat}</h2>
-              <h2>{item.lon}</h2>
-              <h2>{item.description}</h2>
-              <h2>{item.datetime}</h2>
 
+        {
+          this.state.showData &&
+          <Location city_name={this.state.city_name}
+            type={this.state.type}
+            lat={this.state.lat}
+            lon={this.state.lon}
+            map={this.state.map}
+          />
 
-
-              </>
-            })
-          }
-        { this.state.errorMessage &&
-  <h3 className="error"> { this.state.errorMessage } </h3> }
+        }
+      
+        {this.state.errorMessage &&
+          <h3 className="error"> {this.state.errorMessage} </h3>}
       </div>
     )
   }
 }
 
 export default App
+
